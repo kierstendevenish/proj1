cc6ee564638bf076da139d0b1624d59d6f63cb64
Kiersten <kiersten@example.com>
2013-04-15 18:29:20 -0600

    view spacing

--- CodeIgniter_2.1.3/application/views/login_view.php
+++ CodeIgniter_2.1.3/application/views/login_view.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <body>
-   <h2>Login to your account:</h2><br>
-   <?php echo validation_errors(); ?>
    <?php echo form_open('findUser'); ?>
         <label for="findUsername">Find a User:</label>
         <input type="text" size="20" id="findUsername" name="findUsername"/>
         <br/>
    </form><br/><br/>
+    <h2>Login to your account:</h2><br>
+   <?php echo validation_errors(); ?>
    <?php echo form_open('verifylogin'); ?>
      <label for="username">Username:</label>
      <input type="text" size="20" id="username" name="username"/>
